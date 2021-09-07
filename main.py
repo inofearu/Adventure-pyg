70392d3b0d71035dd471fee32267df1a307b9b07
Inofearu <40524038+inofearu@users.noreply.github.com>
2021-09-07 10:53:18 +0000

    temp

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def sleep():
     time.sleep(t)
 
 # screen stuff
-width, height = 1000,600
-backgroundColor = 0,0,0
-screen = pygame.display.set_mode((1000, 600))
-
-while True:
-  screen.fill(backgroundColor)
-  pygame.display.flip()
-
+pygame.init()
+width, height = 800, 600
+backgroundColor = 0,  0,  0
+screen = pygame.display.set_mode((width, height))
+screen_start = 1
+if screen_start == 1:
+	screen.fill(backgroundColor)
+	pygame.display.flip()
 # Difficulties
 
 # Creative Mode
